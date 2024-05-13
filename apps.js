3fb013b4119dbb306552bc87354031bc820464ba
LS_Razon <mdrazon20046@gmail.com>
2024-05-13 23:31:27 +0600

    ToDo

--- apps.js
+++ apps.js
@@ -5,7 +5,7 @@ const toDoList = document.getElementById('todo-list');
 
 function addList(e, t) {
   e.preventDefault();
-  console.log(toDoValue.value);
+  // console.log(toDoValue.value);
   const id = Date.now();
   todoListContiner.push({ id: id, value: toDoValue.value });
   toDoList.innerHTML = '';
@@ -27,8 +27,27 @@ function deleteList(id) {
 
 function randerTodoList() {
   todoListContiner.forEach(function (list) {
-    const html = `<li style="color: #1b6392; font-size: 46px; margin-top: 20px; line-height: 39px; font-weight: 700 ; display: flex; flex-direction: column; background-color: #e0f2fe; border-radius: 16px; padding: 20px 0; border: 8px solid #7dd3fc; width: 500px; text-align: center;">${list.value} <button  style="font-size: 21px; padding: 0 20px; border: 3px solid #1e1e1e; margin: 10px auto 0; border-radius: 10px; color: black; background-color: white; line-height: 21px; margin-top: 72px;"
-    onclick="deleteList(${list.id})">Delete</button> </li>`;
+    const html = `<li style="color: #1b6392; font-size: 46px; margin-top: 20px; line-height: 39px; font-weight: 700 ; display: flex; flex-direction: column; background-color: #e0f2fe; border-radius: 16px; padding: 20px 0; border: 8px solid #7dd3fc; width: 500px; text-align: center;">${list.value} <button style="font-size: 21px; padding: 0 20px; border: 3px solid #1e1e1e; margin: 10px auto 0; border-radius: 10px; color: black; background-color: white; line-height: 21px; margin-top: 72px;" onclick="deleteList(${list.id})">Delete</button>
+    <button style="font-size: 21px; padding: 0 20px; border: 3px solid #1e1e1e; margin: 10px auto 0; border-radius: 10px; color: black; background-color: white; line-height: 21px; margin-top: 72px;" onclick="edit(${list.id},'${list.value}')">Edit</button> </li>`;
     toDoList.insertAdjacentHTML('beforeend', html);
   });
+}
+
+function edit(id, value) {
+  toDoValue.value = value;
+  toDoValue.setAttribute('data-id', id);
+}
+
+function update() {
+  const value = toDoValue.value;
+  const id = toDoValue.getAttribute('data-id' * 1);
+
+  const index = todoListContiner.findIndex(function (toList) {
+    console.log(toList.id);
+    if (toList.id === id) {
+      toList.value = value;
+    }
+    return toList;
+  });
+  console.log(index);
 }
